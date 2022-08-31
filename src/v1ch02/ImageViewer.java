a8594fa1de331188b7a5a3a1e7320455ea9e7bfa
dunkell <49646902+dunkell@users.noreply.github.com>
2022-08-31 23:30:08 +0800

    no
    cmmmit

--- src/v1ch02/ImageViewer.java
+++ src/v1ch02/ImageViewer.java
@@ -1,5 +1,9 @@
 package v1ch02;
 
+import javax.swing.*;
+import java.awt.*;
+import java.io.File;
+
 /**
  * @Auther: Chandler
  * @Date: 2022/8/31 - 08 - 31 - 22:54
@@ -7,5 +11,49 @@ package v1ch02;
  * @version: 1.0
  */
 public class ImageViewer {
+    public static void main(String[] args) {
+        EventQueue.invokeLater(() -> {
+            var frame = new ImageViewerFrame();
+            frame.setTitle("ImageViewer");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setVisible(true);
+        });
+    }
+}
 
+class ImageViewerFrame extends JFrame{
+    private static final int DEFAULT_WIDTH = 300;
+    private static final int DEFAULT_HEIGHT = 400;
+
+    public ImageViewerFrame(){
+        setSize(DEFAULT_WIDTH,DEFAULT_HEIGHT);
+
+        var label = new JLabel();
+        add(label);
+
+        var chooser = new JFileChooser();
+        chooser.setCurrentDirectory(new File("."));
+
+        var menuBar = new JMenuBar();
+        setJMenuBar(menuBar);
+
+        var menu = new JMenu("File");
+        menuBar.add(menu);
+
+        var openItem = new JMenuItem("Open");
+        menu.add(openItem);
+        openItem.addActionListener(event ->{
+            int result = chooser.showOpenDialog(null);
+
+            if(result == JFileChooser.APPROVE_OPTION)
+            {
+                String name = chooser.getSelectedFile().getPath();
+                label.setIcon(new ImageIcon(name));
+            }
+        });
+
+        var exitItem = new JMenuItem("Exit");
+        menu.add(exitItem);
+        exitItem.addActionListener((event -> System.exit(0)));
+    }
 }
